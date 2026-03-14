826bc449241581b3ac5dcc8722eca295d46f45f0
Toru Ogawa <Hakuyume@users.noreply.github.com>
2026-03-15 02:20:02 +0900

    support naive forward proxy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bytes::Bytes;
 use clap::Parser;
-use futures::TryFutureExt;
 use futures::future::Either::{Left as Tcp, Right as Unix};
+use futures::{FutureExt, TryFutureExt};
 use std::convert::Infallible;
 use std::io;
 use std::net::SocketAddr;
@@ -22,8 +22,6 @@ async fn main() -> io::Result<()> {
     tracing_subscriber::fmt::init();
 
     let args = Args::parse();
-    let allow =
-        Arc::new(regex::RegexSet::new(args.allow.iter().map(|allow| allow.as_str())).unwrap());
 
     let mut listeners = futures::future::try_join_all(
         args.bind
@@ -49,6 +47,14 @@ async fn main() -> io::Result<()> {
     }
 
     if !listeners.is_empty() {
+        let state = Arc::new(State {
+            client: hyper_util::client::legacy::Client::builder(
+                hyper_util::rt::TokioExecutor::new(),
+            )
+            .build_http(),
+            allow: regex::RegexSet::new(args.allow.iter().map(|allow| allow.as_str())).unwrap(),
+        });
+
         loop {
             let accept = futures::future::poll_fn(|cx| {
                 for listener in &listeners {
@@ -70,7 +76,7 @@ async fn main() -> io::Result<()> {
             match accept.await? {
                 Tcp((stream, _)) => {
                     let io = hyper_util::rt::TokioIo::new(stream);
-                    let service = service(allow.clone());
+                    let service = service(&state);
                     tokio::spawn(async move {
                         hyper_util::server::conn::auto::Builder::new(
                             hyper_util::rt::TokioExecutor::new(),
@@ -81,7 +87,7 @@ async fn main() -> io::Result<()> {
                 }
                 Unix((stream, _)) => {
                     let io = hyper_util::rt::TokioIo::new(stream);
-                    let service = service(allow.clone());
+                    let service = service(&state);
                     tokio::spawn(async move {
                         hyper_util::server::conn::auto::Builder::new(
                             hyper_util::rt::TokioExecutor::new(),
@@ -97,32 +103,44 @@ async fn main() -> io::Result<()> {
     Ok(())
 }
 
+struct State {
+    client: hyper_util::client::legacy::Client<
+        hyper_util::client::legacy::connect::HttpConnector,
+        hyper::body::Incoming,
+    >,
+    allow: regex::RegexSet,
+}
+
 fn service(
-    allow: Arc<regex::RegexSet>,
+    state: &Arc<State>,
 ) -> impl hyper::service::Service<
     http::Request<hyper::body::Incoming>,
-    Response = http::Response<impl http_body::Body<Data = Bytes, Error = Infallible>>,
+    Response = http::Response<impl http_body::Body<Data = Bytes, Error = hyper::Error> + 'static>,
     Error = Infallible,
     Future: Send,
-> {
+> + 'static {
+    fn status(
+        status: http::StatusCode,
+    ) -> http::Response<http_body_util::combinators::UnsyncBoxBody<Bytes, hyper::Error>> {
+        http::Response::builder()
+            .status(status)
+            .body(http_body_util::combinators::UnsyncBoxBody::default())
+            .unwrap()
+    }
+
+    let state = state.clone();
     let f = move |request: http::Request<hyper::body::Incoming>| {
-        let addr = if request.method() == http::Method::CONNECT {
-            if let (Some(host), Some(port)) = (request.uri().host(), request.uri().port_u16()) {
-                let addr = format!("{host}:{port}");
-                if allow.is_match(&addr) {
-                    Ok(addr)
-                } else {
-                    Err(http::StatusCode::FORBIDDEN)
-                }
-            } else {
-                Err(http::StatusCode::BAD_REQUEST)
-            }
-        } else {
-            Err(http::StatusCode::NOT_FOUND)
-        };
+        let state = state.clone();
         async move {
-            let status = match addr {
-                Ok(addr) => match tokio::net::TcpStream::connect(addr).await {
+            if request.method() == http::Method::CONNECT {
+                let (Some(host), Some(port)) = (request.uri().host(), request.uri().port_u16())
+                else {
+                    return status(http::StatusCode::BAD_REQUEST);
+                };
+                if !state.allow.is_match(&format!("{host}:{port}")) {
+                    return status(http::StatusCode::FORBIDDEN);
+                }
+                match tokio::net::TcpStream::connect((host, port)).await {
                     Ok(mut stream) => {
                         tokio::spawn(
                             async move {
@@ -136,7 +154,9 @@ fn service(
                                         .await
                                         {
                                             Ok((tx, rx)) => tracing::info!(tx, rx),
-                                            Err(e) => tracing::error!(error = e.to_string()),
+                                            Err(e) => {
+                                                tracing::error!(error = e.to_string())
+                                            }
                                         }
                                     }
                                     Err(e) => tracing::error!(error = e.to_string()),
@@ -144,20 +164,37 @@ fn service(
                             }
                             .instrument(tracing::Span::current()),
                         );
-                        http::StatusCode::OK
+                        status(http::StatusCode::OK)
                     }
                     Err(e) => {
                         tracing::error!(error = e.to_string());
-                        http::StatusCode::BAD_GATEWAY
+                        status(http::StatusCode::BAD_GATEWAY)
                     }
-                },
-                Err(status) => status,
-            };
-            Ok(http::Response::builder()
-                .status(status)
-                .body(http_body_util::Empty::new())
-                .unwrap())
+                }
+            } else {
+                let Some(host) = request.uri().host() else {
+                    return status(http::StatusCode::BAD_REQUEST);
+                };
+                let port = match (request.uri().scheme(), request.uri().port_u16()) {
+                    (_, Some(port)) => port,
+                    (Some(scheme), _) if *scheme == http::uri::Scheme::HTTP => 80,
+                    (Some(scheme), _) if *scheme == http::uri::Scheme::HTTPS => 443,
+                    (Some(_), _) => return status(http::StatusCode::BAD_REQUEST),
+                    _ => 80,
+                };
+                if !state.allow.is_match(&format!("{host}:{port}")) {
+                    return status(http::StatusCode::FORBIDDEN);
+                }
+                match state.client.request(request).await {
+                    Ok(response) => response.map(http_body_util::BodyExt::boxed_unsync),
+                    Err(e) => {
+                        tracing::error!(error = e.to_string());
+                        status(http::StatusCode::BAD_GATEWAY)
+                    }
+                }
+            }
         }
+        .map(Ok)
     };
     let service = tower::ServiceBuilder::new()
         .layer(tower_http::trace::TraceLayer::new_for_http())
